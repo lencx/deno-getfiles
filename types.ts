f044bfa86389196cb9195820d48678c64caceb04
lencx <cxin1314@gmail.com>
2020-06-16 18:36:29 +0800

    refactor: getFiles

--- types.ts
+++ types.ts
@@ -1,14 +1,48 @@
-export interface FileInfo {
-  path: string;
-  filename: string;
-  isFile: boolean;
-  isDirectory: boolean;
-  isSymlink: boolean;
+/**
+ * @author: lencx
+ * @create_at: Jun 16, 2020
+ */
+
+export interface GetFilesOptions {
+  // root directory
+  root: string;
+  include?: string[];
+  exclude?: string[]; // subpath
+  ignore?: string[];
+  // type?: 'file' | 'dir'; // default: file
+  hasInfo?: boolean;
 }
 
-export interface GetFilesArgs {
-  dir: string;
-  include?: string[];
+export interface FindFileOptions {
+  path: string;
+  collect: any[];
   exclude?: string[];
   ignore?: string[];
+}
+
+export interface FileInfoDetail {
+  // true if this is info for a regular file
+  isFile: boolean;
+  // true if this is info for a regular directory
+  isDirectory: boolean;
+  // true if this is info for a symlink
+  isSymlink: boolean;
+  // the size of the file, in bytes
+  size: number;
+  // format file size
+  fmtSzie: string;
+  // the last modification time of the file
+  mtime: Date | null;
+  // the last access time of the file
+  atime: Date | null;
+  // the creation time of the file
+  birthtime: Date | null;
+}
+
+export interface FileInfo {
+  path: string;
+  // current working directory
+  cwd: string;
+  filename: string;
+  info: FileInfoDetail;
 }
